15ac887bbf4101deaee5bdbee722975cac001447
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-08-16 21:14:50 +0300

    second

--- hello.c
+++ hello.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
+ 
+/* function declaration */
+void func(void);
+ 
+static int count = 5; /* global variable */
+ 
+main() {
 
-	printf("Hello world\n");
-	return 0;
+   while(count--) {
+      func();
+   }
+	
+   return 0;
+}
 
+/* function definition */
+void func( void ) {
 
+   static int i = 5; /* local static variable */
+   i++;
+
+   printf("i is %d and count is %d\n", i, count);
 }
